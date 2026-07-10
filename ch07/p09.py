a2af14bd872f820efc4ff9668bdc03b8fb528f80
charlesgao <63110137@qq.com>
2026-07-10 22:18:27 +0800

    测试文件操作

---
+++ ch07/p09.py
@@ -0,0 +1,16 @@
+# myfile = open('myfile.txt', 'w')
+# myfile.write('Hello text file\n')
+# myfile.write('goodbye text file\n')
+# myfile.close()
+
+# myfile = open('myfile.txt')
+# print(myfile.readline())
+# print(myfile.readline())
+# print(myfile.readline())
+# myfile.close()
+# print('done')
+
+myfile = open('myfile.txt')
+print(myfile.readlines())
+
+
