d27f0d853bd147131c88aab1166cb2af41ac16bb
RobinGaoofficial <robingao.official@gmail.com>
2026-07-12 01:26:40 +0800

    文件操作

---
+++ python2/ch07/071.py
@@ -0,0 +1,16 @@
+import os
+currentdir = os.path.dirname(__file__) + '/'
+f = open(currentdir + 'cpi1.csv','r+', encoding = 'utf-8')
+
+# f = open('E:\\sourcecode\\cl2\\python2\\ch07\\cpi1.csv','r+', encoding = 'utf-8')
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# f.write('早上好00112233\n')
+# f.write('晚上好\n')
+s = f.read()
+print(s)
+f.seek(0)
+ls = f.readlines()
+print(ls)
+f.close()
