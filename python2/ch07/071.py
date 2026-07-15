77b48213f5200798463723f0f4322ec67df83e30
RobinGaoofficial <robingao.official@gmail.com>
2026-07-15 21:45:35 +0800

    test

--- python2/ch07/071.py
+++ python2/ch07/071.py
@@ -1,16 +1,19 @@
-import os
-currentdir = os.path.dirname(__file__) + '/'
-f = open(currentdir + 'cpi1.csv','r+', encoding = 'utf-8')
+# import os
+# currentdir = os.path.dirname(__file__) + '/'
+# f = open(currentdir + 'cpi1.csv','r+', encoding = 'utf-8')
 
-# f = open('E:\\sourcecode\\cl2\\python2\\ch07\\cpi1.csv','r+', encoding = 'utf-8')
-# print(f.readline())
-# print(f.readline())
-# print(f.readline())
-# f.write('早上好00112233\n')
-# f.write('晚上好\n')
-s = f.read()
-print(s)
-f.seek(0)
-ls = f.readlines()
-print(ls)
-f.close()
+# # f = open('E:\\sourcecode\\cl2\\python2\\ch07\\cpi1.csv','r+', encoding = 'utf-8')
+# # print(f.readline())
+# # print(f.readline())
+# # print(f.readline())
+# # f.write('早上好00112233\n')
+# # f.write('晚上好\n')
+# s = f.read()
+# print(s)
+# f.seek(0)
+# ls = f.readlines()
+# print(ls)
+# f.close()
+
+a = print("Hello World")
+a == None
